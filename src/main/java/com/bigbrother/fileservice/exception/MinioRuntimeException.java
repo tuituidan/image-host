072b141e702cbbd65364b8299a3dae271aa14716
zhujunhan <136375002@qq.com>
2020-08-01 10:20:11 +0800

    替换

--- src/main/java/com/bigbrother/fileservice/exception/MinioRuntimeException.java
+++
@@ -1,8 +0,0 @@
-package com.bigbrother.fileservice.exception;
-
-/**
- * @author Administrator
- */
-public class MinioRuntimeException extends RuntimeException {
-    private static final long serialVersionUID = 6926628857663795535L;
-}
