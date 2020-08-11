411f382a0b1a6007ea5365b5b15e2aa7f1612520
zhujunhan <136375002@qq.com>
2020-08-11 20:17:10 +0800

    脱敏

--- docs/libs/webuploader/uploader.js
+++ docs/libs/webuploader/uploader.js
@@ -41,7 +41,7 @@
             // 允许上传的文件格式，主要是图片、音视频和浏览器能直接显示的文件类型等.
             acceptExt: "jpg,jpeg,gif,png,bmp,svg,psd,tif,webp,ico,flv,swf,avi,mov,wmv,mp3,mp4,wav,pdf,json,html,js,css,xml,md,txt",
             autoload: false, // 选择文件后是否自动上传
-            clientId: "thunisoft-ibed-" + WebUploader.Base.guid(),//随机的客户端唯一标识
+            clientId: "image-host-" + WebUploader.Base.guid(),//随机的客户端唯一标识
             promptText: "",//上传控件下显示自定义提示信息，如上传大小，个数之类的
             deleteCallback: undefined //触发删除的回调函数，做一些删除处理
         };
